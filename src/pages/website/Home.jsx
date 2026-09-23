dcc40d828a38e6b8067f08ee5b34a8c25b091d24
KashifKhalil986 <kashifkhalil720@gmail.com>
2026-09-23 16:04:25 +0500

    Fix import path for TopDoctor component to match casing

--- src/pages/website/Home.jsx
+++ src/pages/website/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../../components/Header";
 import SpecialityMenu from "../../components/SpecialityMenu";
-import TopDoctor from "../../components/topDoctor";
+import TopDoctor from "../../components/TopDoctor";
 import Banner from "../../components/Banner";
 
 const Home = () => {
